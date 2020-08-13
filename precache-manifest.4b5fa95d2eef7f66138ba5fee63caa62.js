0d0813838b076c8c78e8e2246fb226d5ef870109
caioAugustoRodrigues <caio.turmaxii@gmail.com>
2020-08-12 22:54:28 -0300

    Updates

---
+++ precache-manifest.4b5fa95d2eef7f66138ba5fee63caa62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f8ae40fd7c7229fe4e6f76ca3478bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a595d4d700c1a8a1f6f5",
+    "url": "/static/css/main.a5dc6ed0.chunk.css"
+  },
+  {
+    "revision": "4a1a054c51b5125f5479",
+    "url": "/static/js/2.99f5289a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99f5289a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a595d4d700c1a8a1f6f5",
+    "url": "/static/js/main.c32c5096.chunk.js"
+  },
+  {
+    "revision": "0d03a3ae2386a57ef04f",
+    "url": "/static/js/runtime-main.015849b2.js"
+  }
+]);
